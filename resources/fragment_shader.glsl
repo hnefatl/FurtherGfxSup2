ecb16335508fc500e0684d021013c9b44c30f387
Keith <hnefatl@gmail.com>
2017-10-29 23:09:08 +0000

    Failed. I can only get a "static" wireframe on the cube if I don't rotate the world coordinates, which I can't make sense of.

--- resources/fragment_shader.glsl
+++ resources/fragment_shader.glsl
@@ -11,26 +11,16 @@ varying vec3 world_position;
 
 void main()
 {
-    if (length(world_position) <= 1.3)
-    {
-        colour = vec3(1, 0, 0);
-        return;
-    }
-
-    const float kA = 0.1;
-    const float kD = 0.75;
-
-    const vec3 WHITE = vec3(1, 1, 1);
     const vec3 BLACK = vec3(0, 0, 0);
-    const vec3 BLUE = vec3(0, 0, 1);
 
-    colour = BLACK;
-
-    colour += kA * BLUE;
-
-    vec3 n = normalize(frag_normal);
-    vec3 l = normalize(lightPosition - world_position);
-    float nl = dot(n, l);
-    if (nl > 0)
-        colour += kD * nl * BLUE;
+    // Radius of a circle whose exterior matches the wires of the cube
+    const float circleRadius = 1.379;
+    if (length(world_position.xy) >= circleRadius ||
+        length(world_position.yz) >= circleRadius ||
+        length(world_position.xz) >= circleRadius)
+    {
+        colour = world_position;
+    }
+    else
+        discard;
 }
